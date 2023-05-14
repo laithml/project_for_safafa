caef80b9116722aa7711a53afe0fa97f333ea120
Chris-Haj <chrishaj1191@gmail.com>
2023-05-14 17:28:45 +0300

    split

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,5 @@
 import { Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Spacing from "../constants/Spacing";
 import FontSize from "../constants/FontSize";
@@ -9,84 +9,115 @@ import { TextInput } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
+const header = function createAccountHeader() {
+  return (
+    <View style={{ alignItems: "center" }}>
+      <Text
+        style={{
+          fontSize: FontSize.xLarge,
+          color: Colors.primary,
+          marginVertical: Spacing * 2,
+          FontFamily: Font["poppins-semiBold"],
+        }}
+      >
+        Create Account
+      </Text>
+      <Text
+        style={{
+          FontFamily: Font["poppins-semiBold"],
+          fontSize: FontSize.medium,
+          maxWidth: "60%",
+          textAlign: "center",
+        }}
+      >
+        Create an account to get all features
+      </Text>
+    </View>
+  );
+};
+
+function createNameInput(name) {
+  return (
+    <TextInput
+      placeholder="Full Name"
+      placeholderTextColor={Colors.darkText}
+      style={{
+        borderWidth: 1,
+        backgroundColor: Colors.lightPrimary,
+        borderRadius: Spacing,
+        padding: Spacing * 2,
+        marginVertical: Spacing,
+        borderColor: Colors.gray,
+      }}
+      onChangeText={() => {
+        if (name.length > 5) console.log("Too long");
+        else console.log("good");
+      }}
+    />
+  );
+}
+function createEmailInput() {
+  return (
+    <TextInput
+      placeholder="Email"
+      placeholderTextColor={Colors.darkText}
+      style={{
+        borderWidth: 1,
+        backgroundColor: Colors.lightPrimary,
+        borderRadius: Spacing,
+        padding: Spacing * 2,
+        marginVertical: Spacing,
+        borderColor: Colors.gray,
+      }}
+    />
+  );
+}
+function createPhoneInput() {
+  return (
+    <TextInput
+      placeholder="Phone Number"
+      placeholderTextColor={Colors.darkText}
+      style={{
+        borderWidth: 1,
+        backgroundColor: Colors.lightPrimary,
+        borderRadius: Spacing,
+        padding: Spacing * 2,
+        marginVertical: Spacing,
+        borderColor: Colors.gray,
+      }}
+    />
+  );
+}
+function createPasswordInput() {
+  return (
+    <TextInput
+      placeholder="Password"
+      placeholderTextColor={Colors.darkText}
+      secureTextEntry={true}
+      style={{
+        borderWidth: 1,
+        backgroundColor: Colors.lightPrimary,
+        borderRadius: Spacing,
+        padding: Spacing * 2,
+        marginVertical: Spacing,
+        borderColor: Colors.gray,
+      }}
+    />
+  );
+}
+
 export default function SignUp() {
   const navigation = useNavigation();
+  const [name, mail, number, pass] = React.useState("");
   return (
     <SafeAreaView>
       <View style={{ padding: Spacing }}>
-        <View style={{ alignItems: "center" }}>
-          <Text
-            style={{
-              fontSize: FontSize.xLarge,
-              color: Colors.primary,
-              marginVertical: Spacing * 2,
-              FontFamily: Font["poppins-semiBold"],
-            }}
-          >
-            Create Account
-          </Text>
-          <Text
-            style={{
-              FontFamily: Font["poppins-semiBold"],
-              fontSize: FontSize.medium,
-              maxWidth: "60%",
-              textAlign: "center",
-            }}
-          >
-            Create an account to get all features
-          </Text>
-        </View>
-        <View style={{ marginVertical: Spacing }}>
-          <TextInput
-            placeholder="Full Name"
-            placeholderTextColor={Colors.darkText}
-            style={{
-              borderWidth: 1,
-              backgroundColor: Colors.lightPrimary,
-              borderRadius: Spacing,
-              padding: Spacing * 2,
-              marginVertical: Spacing,
-              borderColor: Colors.gray,
-            }}
-          />
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor={Colors.darkText}
-            style={{
-              borderWidth: 1,
-              backgroundColor: Colors.lightPrimary,
-              borderRadius: Spacing,
-              padding: Spacing * 2,
-              marginVertical: Spacing,
-              borderColor: Colors.gray,
-            }}
-          />
-          <TextInput
-            placeholder="Phone Number"
-            placeholderTextColor={Colors.darkText}
-            style={{
-              borderWidth: 1,
-              backgroundColor: Colors.lightPrimary,
-              borderRadius: Spacing,
-              padding: Spacing * 2,
-              marginVertical: Spacing,
-              borderColor: Colors.gray,
-            }}
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor={Colors.darkText}
-            secureTextEntry={true}
-            style={{
-              borderWidth: 1,
-              backgroundColor: Colors.lightPrimary,
-              borderRadius: Spacing,
-              padding: Spacing * 2,
-              marginVertical: Spacing,
-              borderColor: Colors.gray,
-            }}
-          />
-        </View>
+        <View style={{ alignItems: "center" }}>{header()}</View>
+        <View style={{ marginVertical: Spacing }}></View>
+        {createNameInput(name)}
+        {createEmailInput(mail)}
+        {createPhoneInput(number)}
+        {createPasswordInput(pass)}
         <TouchableOpacity
           style={{
             padding: Spacing * 2,

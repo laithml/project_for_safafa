05d3cca2a8faab2cf227cc3d1d78e1e2642dc735
MoeYousef1 <mohammadyousef08@gmail.com>
2023-06-16 01:51:43 +0300

    added a popup to event page

--- components/eventCard.js
+++ components/eventCard.js
@@ -1,19 +1,73 @@
-import React from "react";
-import { Text, Image, View, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {Text, Image, View, StyleSheet, TouchableOpacity, Modal, ImageBackground, ScrollView} from "react-native";
 import Colors from "../constants/Colors";
 
 export function EventCard({ name, description, img, onPress }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => setModalVisible(!modalVisible);
+
+  const [ ,setIsScrollable] = useState(false);
+  const [layoutHeight, setLayoutHeight] = useState(0);
+
+  const handleScroll = (event) => {
+    const contentHeight = event.nativeEvent.contentSize.height;
+    setIsScrollable(contentHeight > layoutHeight);
+  };
+
+  const handleLayout = (event) => {
+    const layoutHeight = event.nativeEvent.layout.height;
+    setLayoutHeight(layoutHeight);
+  };
+
   return (
-      <TouchableOpacity style={styles.card} onPress={onPress}>
-        <Image style={styles.image} src={img} />
-        <View style={styles.infoContainer}>
-          <View style={styles.textContainer}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.description}>{description}</Text>
+      <View>
+        <TouchableOpacity style={styles.card} onPress={toggleModal}>
+          <Image style={styles.image} source={{ uri: img }} />
+          <View style={styles.infoContainer}>
+            <View style={styles.textContainer}>
+              <Text style={styles.name}>{name}</Text>
+            </View>
+            <View style={styles.line} />
+            <TouchableOpacity
+                style={styles.readButton}
+                onPress={toggleModal}
+            >
+              <Text style={styles.readButtonText}>Read about the event</Text>
+            </TouchableOpacity>
           </View>
-          <View style={styles.line} />
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={toggleModal}
+        >
+          <View style={styles.centeredView}>
+            <ImageBackground
+                style={styles.modalView}
+                source={{ uri: img }}
+            >
+              <View style={styles.overlay} />
+              <ScrollView
+                  contentContainerStyle={styles.scrollView}
+                  onScroll={handleScroll}
+                  onLayout={handleLayout}
+                  scrollEventThrottle={400}
+              >
+                <Text style={styles.modalTextLarge}>{description}</Text>
+              </ScrollView>
+
+              <TouchableOpacity
+                  style={{ ...styles.openButton, backgroundColor: Colors.primary }}
+                  onPress={toggleModal}
+              >
+                <Text style={styles.textStyle}>Close</Text>
+              </TouchableOpacity>
+            </ImageBackground>
+          </View>
+        </Modal>
+      </View>
   );
 }
 
@@ -46,13 +100,60 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 8,
   },
-  description: {
-    fontSize: 16,
-    fontWeight: "600",
-    textAlign: "center",
-  },
   line: {
     borderBottomColor: Colors.lightPrimary,
     borderBottomWidth: 5,
+  },
+  readButton: {
+    backgroundColor: Colors.primary,
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  readButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    width: 400,
+    height: 400,
+    alignItems: "center",
+    justifyContent: 'space-between',
+    padding: 15,
+    backgroundColor: 'transparent',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    justifyContent: 'center',
+    padding: 30,
+  },
+  openButton: {
+    backgroundColor: Colors.primary,
+    borderRadius: 20,
+    padding: 10,
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalTextLarge: {
+    fontSize: 16,
+    textAlign: 'center',
+    paddingHorizontal: 30,
+  },
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'center',
   },
 });
